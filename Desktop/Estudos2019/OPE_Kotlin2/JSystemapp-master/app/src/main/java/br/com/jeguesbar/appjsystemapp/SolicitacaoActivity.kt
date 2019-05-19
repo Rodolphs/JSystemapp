590de7c22bf3b1744f7bd052b9e27dac5e03f98e
vgerace <vitorgerace@gmail.com>
2019-05-18 23:17:40 -0300

    Alteracoes no gradle

---
+++ Desktop/Estudos2019/OPE_Kotlin2/JSystemapp-master/app/src/main/java/br/com/jeguesbar/appjsystemapp/SolicitacaoActivity.kt
@@ -0,0 +1,12 @@
+package br.com.jeguesbar.appjsystemapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SolicitacaoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_solicitacao)
+    }
+}
